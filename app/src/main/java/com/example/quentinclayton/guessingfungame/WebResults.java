45c825c2d96ad0073e59d5283886fa2627ae9f87
qclayton <qclayton@student.neumont.edu>
2018-08-01 13:20:07 -0600

    Added the second activity and webResults that will be needed.

---
+++ app/src/main/java/com/example/quentinclayton/guessingfungame/WebResults.java
@@ -0,0 +1,25 @@
+package com.example.quentinclayton.guessingfungame;
+
+import android.graphics.Bitmap;
+
+public class WebResults {
+
+    String title;
+    Bitmap image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+}
